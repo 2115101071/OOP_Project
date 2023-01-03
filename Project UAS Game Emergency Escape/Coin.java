f44df93f44641eb89f579fe5711e9d80dd64ce17
Yuda Mahendra <115060091+2115101071@users.noreply.github.com>
2023-01-03 14:34:35 +0800

    first

---
+++ Project UAS Game Emergency Escape/Coin.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Coin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Coin extends Actor
+{
+    /**
+     * Act - do whatever the Coin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Coin(){
+        GreenfootImage myImage = getImage();
+        int myNewHeight = (int)myImage.getHeight()/10;
+        int myNewWidth = (int)myImage.getWidth()/10;
+        myImage.scale(myNewWidth, myNewHeight);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
